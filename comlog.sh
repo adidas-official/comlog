599904c8b743cf471776b075c49b8266c1b0fabb
spartakus <spartakus@gmail.com>
2020-09-07 06:58:15 -0400

    Zakladni funkce pro pridavani prikazu do array

---
+++ comlog.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+commands=()
+while true; do
+	read -p "$" comm
+	if [ ! -z $comm ]; then
+		commands+=($comm)
+	else
+		break
+	fi
+done
+
+for i in ${commands[@]};do
+	echo $i
+done
