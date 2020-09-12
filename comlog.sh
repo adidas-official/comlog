304e4b993549d4e9ebcdb191d44fd8a81547599f
Bob Karma <bobkarma@MacBook-Pro.local>
2020-09-12 10:28:39 +0200

    Rozdeleni kodu do funkci.

--- comlog.sh
+++ comlog.sh
@@ -1,29 +1,50 @@
 #! /bin/bash
 # Description: App for logging commands
 
-commands=()
-logsdir="/home/spartakus/scripts/comlog/logs"
+#logsdir="/home/spartakus/scripts/comlog/logs"
 
-while true; do
-	read -p "$" comm
-	if [ ! -z "$comm" ]; then
-		commands+=("$comm")
-	else
-		break
-	fi
-done
+logs_where () {
+    read -p "Logs directory: " logsdir
+    if [ ! -f $logsdir ]; then
+        mkdir -p $logsdir
+    fi
+    echo $logsdir
+}
 
-if [ ! -f $logsdir ]; then
-	mkdir -p $logsdir
-fi
+show_dir () {
+    cd $1
+    pwd
+    ls -lh
+}
 
-cd $logsdir;
-ls -l;
-read -p "Name of log file: " logname
-touch $logname.txt
-chmod 644 $logname.txt
+make_logfile () {
+    read -p "Name of log file: " logname
+    touch $logname.txt
+    chmod 644 $logname.txt
+    echo $logname
+}
 
-for i in "${commands[@]}";do
-	echo $i >> $logname.txt;
-	#$i
-done
+main () {
+
+    lw=$(logs_where)
+    show_dir $lw
+    logfile=$(make_logfile)
+
+    # Create commands
+    commands=()
+    while true; do
+        read -p "$ " comm
+        if [ ! -z "$comm" ]; then
+            commands+=("$comm")
+        else
+            break
+        fi
+    done
+
+    # Write them to log file
+    for i in "${commands[@]}";do
+        echo $i >> $logfile.txt;
+    done
+}
+
+main
